71d0274b6c6ea7964a7613186b35d7e6c8d9b38b
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 07:21:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,14 +64,14 @@ def monopoly_with_fourth_tier
           :mortgage_value => 100
         },
         :pennsylvania_railroad => {
-          :mortage_value => 200
+          :mortgage_value => 200
         },
         :b_and_o_railroad => {
-          :mortage_value => 400
+          :mortgage_value => 400
           
         },
         :shortline_railroad => {
-          :mortage_value => 800
+          :mortgage_value => 800
           
         }
       }
