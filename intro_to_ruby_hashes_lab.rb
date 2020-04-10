654f9ebb8e04d612b00a8a217b8ba1985bfa9ca9
Jesse Langley <jlangleyiii@aol.com>
2020-04-10 07:06:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,10 +59,10 @@ def monopoly_with_fourth_tier
            :four_pieces_owned => 200
       },
       :names => {
-          :reading_railroad => {:mortage_value => 100},
-          :pennsylvania_railroad => {:mortage_value =>},
-           :b_and_o_railroad => {:mortage_value =>},
-           :shortline_railroad => {}
+        :reading_railroad => {:mortage_value => 100},
+        :pennsylvania_railroad => {:mortage_value => 200},
+        :b_and_o_railroad => {:mortage_value => 400},
+        :shortline_railroad => {:mortage_value =>800}
            
            # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
